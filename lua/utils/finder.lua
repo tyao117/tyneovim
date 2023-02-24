5efcbbc7deb55af4d12c751ff3e2cba0d478dcf2
Timothy-Yao <timothy.yao@ibm.com>
2023-02-23 20:23:27 -0600

    replace the copy with the function i do want

--- lua/utils/finder.lua
+++ lua/utils/finder.lua
@@ -30,13 +30,13 @@ function M.find_buffers()
   end)
 end
 
-function M.find_files()
-  local fzf = require "fzf-lua"
-  if vim.fn.system "git rev-parse --is-inside-work-tree" == true then
-    fzf.git_files()
-  else
-    fzf.files()
-  end
+-- Find dotfiles
+function M.find_dotfiles()
+  require("telescope.builtin").find_files {
+    prompt_title = "<Dotfiles>",
+    cwd = "$HOME",
+  }
 end
+
 
 return M
